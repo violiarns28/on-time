c2a7d469e0700d32c17e52851897d9bfffd5e672
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-04-15 13:55:52 +0700

    fix: update BASE_URL to use production endpoint in post script

--- bench/post.sh
+++ bench/post.sh
@@ -3,8 +3,8 @@
 # set environment variables for k6
 ACCESS_TOKEN="eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MjAwMDAzLCJlbWFpbCI6ImFzZGZAYXNkZi5jb20iLCJuYW1lIjoiYXNkZmFzIiwiZGV2aWNlSWQiOiJkZXZpY2VJZCIsImlhdCI6MTc0NDY5ODk1NX0.GFt5sms-KbR6nYiExXc7PZ0WzI5cHc09XA5yNE8qhA4"
 
-# BASE_URL="https://attendance-api.zenta.dev"
-BASE_URL="http://localhost:3000"
+BASE_URL="https://attendance-api.zenta.dev"
+# BASE_URL="http://localhost:3000"
 
 # run post attendance
 echo "Running post attendance"
