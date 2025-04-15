4e8b1d5cd784bcc33ebf0cf95132e294a2fabb2c
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-04-16 05:36:52 +0700

    fix: update K6_WEB_DASHBOARD_EXPORT filename for consistency in report generation

--- bench/post.sh
+++ bench/post.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 
 echo "Running post attendance"
-K6_WEB_DASHBOARD=true K6_WEB_DASHBOARD_EXPORT=attendance_simulation_report.html k6 run ./attendance/post.js
+K6_WEB_DASHBOARD=true K6_WEB_DASHBOARD_EXPORT=report.html k6 run ./attendance/post.js
 echo "Done running post attendance"
 
