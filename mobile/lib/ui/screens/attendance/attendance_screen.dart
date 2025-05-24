0b670547556197bd591fed9220174574f5a0efa2
violia ruana <violiaruana@gmail.com>
2025-05-24 11:58:24 +0700

    style: 💄 restyling the ui in attendance menu

--- mobile/lib/ui/screens/attendance/attendance_screen.dart
+++ mobile/lib/ui/screens/attendance/attendance_screen.dart
@@ -11,230 +11,190 @@ class AttendanceScreen extends GetView<AttendanceController> {
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
-    final formHeight = height - 339;
+    final formHeight = height - 135;
 
     return Scaffold(
-      backgroundColor: const Color(0xFF96D4E1),
-      body: Stack(
+      backgroundColor: Colors.white,
+      body: Column(
         children: [
-          Column(
-            children: [
-              const Padding(
-                padding: EdgeInsets.fromLTRB(16, 48, 16, 48),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [],
-                ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [],
+          ),
+          Container(
+            alignment: Alignment.center,
+            width: double.infinity,
+            height: height / 10,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment(0.8, 1),
+                colors: <Color>[
+                  Color(0xFF0E5D6D),
+                  Color(0xFF3C94A5),
+                ],
               ),
-              Center(
-                child: Column(
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.only(top: 189),
-                      padding: const EdgeInsets.fromLTRB(16, 56, 16, 24),
-                      width: double.infinity,
-                      height: formHeight,
-                      color: Colors.white,
-                      child: Column(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(50),
+                topLeft: Radius.circular(0),
+                bottomRight: Radius.circular(50),
+                topRight: Radius.circular(0),
+              ),
+            ),
+            child: Text(
+              "Attendance",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w600),
+            ),
+          ),
+          Center(
+            child: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.fromLTRB(16, 24, 16, 24),
+                  width: double.infinity,
+                  height: formHeight,
+                  color: Colors.white,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
                         crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Obx(
-                                () => _buildClockContainer(
-                                    "Clock In",
-                                    controller.todayClockInTime,
-                                    const Color(0xFF2DBF4D)),
-                              ),
-                              const SizedBox(width: 16.0),
-                              Obx(
-                                () => _buildClockContainer(
-                                    "Clock Out",
-                                    controller.todayClockOutTime,
-                                    const Color(0xFFE74C3C)),
-                              ),
-                            ],
+                          Obx(
+                            () => _buildClockContainer(
+                                "Clock In",
+                                controller.todayClockInTime,
+                                const Color(0xFF2DBF4D)),
                           ),
-                          const SizedBox(height: 24),
-                          SizedBox(
-                            width: 300,
-                            height: 42,
-                            child: Obx(
-                              () => ElevatedButton(
-                                onPressed: (controller.isLoading ||
-                                        controller.isTodayClockInAndOut)
-                                    ? null
-                                    : () {
-                                        final controller =
-                                            Get.find<AttendanceController>();
-                                        controller.saveAttendance(context);
-                                      },
-                                style: ButtonStyle(
-                                  backgroundColor:
-                                      WidgetStateProperty.all<Color>(
-                                    controller.isTodayClockInAndOut
-                                        ? const Color(0xFF4098AA)
-                                            .withOpacity(0.5)
-                                        : const Color(0xFF4098AA),
-                                  ),
-                                ),
-                                child: controller.isLoading
-                                    ? const SizedBox(
-                                        height: 20,
-                                        width: 20,
-                                        child: CircularProgressIndicator(
-                                          valueColor:
-                                              AlwaysStoppedAnimation<Color>(
-                                                  Colors.white),
-                                        ),
-                                      )
-                                    : Text(
-                                        controller.isTodayClockInAndOut
-                                            ? "Already Presence Today"
-                                            : controller.isTodayClockIn
-                                                ? "Clock Out"
-                                                : "Clock In",
-                                        style: const TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 18.0,
-                                          fontWeight: FontWeight.w600,
-                                        ),
-                                      ),
+                          const SizedBox(width: 16.0),
+                          Obx(
+                            () => _buildClockContainer(
+                                "Clock Out",
+                                controller.todayClockOutTime,
+                                const Color(0xFFE74C3C)),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 24),
+                      SizedBox(
+                        width: 300,
+                        height: 42,
+                        child: Obx(
+                          () => ElevatedButton(
+                            onPressed: (controller.isLoading ||
+                                    controller.isTodayClockInAndOut)
+                                ? null
+                                : () {
+                                    final controller =
+                                        Get.find<AttendanceController>();
+                                    controller.saveAttendance(context);
+                                  },
+                            style: ButtonStyle(
+                              backgroundColor: WidgetStateProperty.all<Color>(
+                                controller.isTodayClockInAndOut
+                                    ? const Color(0xFF4098AA).withOpacity(0.5)
+                                    : const Color(0xFF4098AA),
                               ),
                             ),
+                            child: controller.isLoading
+                                ? const SizedBox(
+                                    height: 20,
+                                    width: 20,
+                                    child: CircularProgressIndicator(
+                                      valueColor: AlwaysStoppedAnimation<Color>(
+                                          Colors.white),
+                                    ),
+                                  )
+                                : Text(
+                                    controller.isTodayClockInAndOut
+                                        ? "Already Presence Today"
+                                        : controller.isTodayClockIn
+                                            ? "Clock Out"
+                                            : "Clock In",
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
                           ),
-                          const SizedBox(height: 8),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              const Text(
-                                "History",
-                                style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 18.0,
-                                  fontWeight: FontWeight.w600,
-                                ),
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const Text(
+                            "History",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          TextButton(
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12.0),
                               ),
-                              TextButton(
-                                style: ElevatedButton.styleFrom(
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(12.0),
+                            ),
+                            onPressed: () {
+                              Get.to(() => const HistoryDetailScreen(),
+                                  binding: HistoryDetailBinding());
+                            },
+                            child: const Row(
+                              children: [
+                                Text(
+                                  "See all",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.normal,
                                   ),
                                 ),
-                                onPressed: () {
-                                  Get.to(() => const HistoryDetailScreen(),
-                                      binding: HistoryDetailBinding());
-                                },
-                                child: const Row(
-                                  children: [
-                                    Text(
-                                      "See all",
-                                      style: TextStyle(
-                                        color: Colors.black,
-                                        fontSize: 16.0,
-                                        fontWeight: FontWeight.normal,
-                                      ),
-                                    ),
-                                    SizedBox(width: 8),
-                                    Icon(
-                                      Icons.arrow_forward_ios_rounded,
-                                      size: 16,
-                                      color: Colors.black,
-                                    ),
-                                  ],
+                                SizedBox(width: 8),
+                                Icon(
+                                  Icons.arrow_forward_ios_rounded,
+                                  size: 16,
+                                  color: Colors.black,
                                 ),
-                              ),
-                            ],
-                          ),
-                          Expanded(
-                            child: Obx(
-                              () => SingleChildScrollView(
-                                child: Column(
-                                  children:
-                                      controller.attendances.map((attendance) {
-                                    return _buildHistoryItem(
-                                      attendance.userName,
-                                      attendance.previousHash,
-                                      attendance.hash,
-                                      showClockIn: attendance.type ==
-                                          AttendanceType.CLOCK_IN,
-                                      showClockOut: attendance.type ==
-                                          AttendanceType.CLOCK_OUT,
-                                      timestamp: attendance.timestamp,
-                                    );
-                                  }).toList(),
-                                ),
-                              ),
+                              ],
                             ),
                           ),
                         ],
                       ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-          Positioned(
-            top: 250.0,
-            left: 0,
-            right: 0,
-            child: Center(
-              child: Container(
-                width: 300,
-                height: height / 11.4,
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment(0.8, 1),
-                    colors: <Color>[
-                      Color(0xFF0E5D6D),
-                      Color(0xFF3C94A5),
+                      Expanded(
+                        child: Obx(
+                          () => SingleChildScrollView(
+                            child: Column(
+                              children:
+                                  controller.attendances.map((attendance) {
+                                return _buildHistoryItem(
+                                  attendance.userName,
+                                  attendance.previousHash,
+                                  attendance.hash,
+                                  showClockIn: attendance.type ==
+                                      AttendanceType.CLOCK_IN,
+                                  showClockOut: attendance.type ==
+                                      AttendanceType.CLOCK_OUT,
+                                  timestamp: attendance.timestamp,
+                                );
+                              }).toList(),
+                            ),
+                          ),
+                        ),
+                      ),
                     ],
                   ),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(15),
-                  ),
                 ),
-                child: const Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Icon(
-                        Icons.location_on,
-                        color: Colors.white,
-                        size: 35,
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Location",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.w600),
-                        ),
-                        Text(
-                          "Ketintang, Surabaya",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 14.0,
-                              fontWeight: FontWeight.normal),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
+              ],
             ),
           ),
         ],
